76d14ea492fbeb9871441a3c963f5a7cede1efea
Igor Kopeikin <ikopeikin@alpha-fsw.com>
2024-07-03 14:56:34 -0400

    curl call

--- run_curl.cmd
+++ run_curl.cmd
@@ -1,1 +1,1 @@
-curl curl -v -H "authorization: Bearer eyJhAMobiD9A" "http://edatapole.com/api/messages/bsns?st=NY&after=2024-06-15&new=1" 
+curl -v -H "authorization: Bearer eyJhAMobiD9A" "http://edatapole.com/api/messages/bsns?st=NY&after=2024-06-15&new=1" 
